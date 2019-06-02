207a0ade1fda2118e1ccdea76ff6621146a2257c
abdullaqudrat <aqudrat630@gmail.com>
2019-06-02 11:18:54 -0600

    Adds user email req format test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,12 +27,26 @@ RSpec.describe User, type: :model do
       end
     end
     context "validate social security number limitations" do
-      it "is invalid with LESS than 9 characteras" do 
+      it "is invalid with LESS than 9 characters" do 
         user = User.new(first_name: "Abdulla", last_name: "Qudrat", email: "aqudrat638@gmail.com", social_security_number: "55555555")
         expect(user).to be_invalid
       end
       it "is invalid with MORE than 9 characters" do 
         user = User.new(first_name: "Abdulla", last_name: "Qudrat", email: "aqudrat638@gmail.com", social_security_number: "5555555555")
+        expect(user).to be_invalid
+      end
+    end
+    context "validate email limitations" do
+      it "is invalid with incorrect email format" do 
+        user = User.new(first_name: "Abdulla", last_name: "Qudrat", email: "aqudrat638gmail.com", social_security_number: "555555555")
+        expect(user).to be_invalid
+      end
+      it "is invalid with incorrect email format" do 
+        user = User.new(first_name: "Abdulla", last_name: "Qudrat", email: "@gmail.com", social_security_number: "555555555")
+        expect(user).to be_invalid
+      end
+      it "is invalid with incorrect email format" do 
+        user = User.new(first_name: "Abdulla", last_name: "Qudrat", email: "aqudrat638@gmail", social_security_number: "555555555")
         expect(user).to be_invalid
       end
     end
