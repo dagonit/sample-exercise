082d58af7f8ca4cfe107239467d969f0b706f634
abdullaqudrat <aqudrat630@gmail.com>
2019-06-02 11:04:29 -0600

    Adds SSN req 9 characters to spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     context "invalid attributes" do
       it "is invalid without a first_name" do
-        user = User.new(last_name: "Qudrat", email: "aqudrat638@gmail.com", social_security_number: "555-55-5555")
+        user = User.new(last_name: "Qudrat", email: "aqudrat638@gmail.com", social_security_number: "555555555")
         expect(user).to be_invalid
       end
       it "is invalid without a last_name" do
-        user = User.new(first_name: "Abdulla", email: "aqudrat638@gmail.com", social_security_number: "555-55-5555")
+        user = User.new(first_name: "Abdulla", email: "aqudrat638@gmail.com", social_security_number: "555555555")
         expect(user).to be_invalid
       end
       it "is invalid without an email" do
-        user = User.new(first_name: "Abdulla", last_name: "Qudrat", social_security_number: "555-55-5555")
+        user = User.new(first_name: "Abdulla", last_name: "Qudrat", social_security_number: "555555555")
         expect(user).to be_invalid
       end
       it "is invalid without a social_security_number" do
@@ -22,8 +22,18 @@ RSpec.describe User, type: :model do
     end
     context "valid attributes" do
       it "is valid with all attributes" do 
-        user = User.new(first_name: "Abdulla", last_name: "Qudrat", email: "aqudrat638@gmail.com", social_security_number: "555-55-5555")
+        user = User.new(first_name: "Abdulla", last_name: "Qudrat", email: "aqudrat638@gmail.com", social_security_number: "555555555")
         expect(user).to be_valid
+      end
+    end
+    context "validate social security number limitations" do
+      it "is invalid with LESS than 9 characteras" do 
+        user = User.new(first_name: "Abdulla", last_name: "Qudrat", email: "aqudrat638@gmail.com", social_security_number: "55555555")
+        expect(user).to be_invalid
+      end
+      it "is invalid with MORE than 9 characters" do 
+        user = User.new(first_name: "Abdulla", last_name: "Qudrat", email: "aqudrat638@gmail.com", social_security_number: "5555555555")
+        expect(user).to be_invalid
       end
     end
   end
