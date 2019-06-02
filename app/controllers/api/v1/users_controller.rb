66a07a6e327f4cb6c7ef0d73d9da2e3379bc32eb
abdullaqudrat <aqudrat630@gmail.com>
2019-06-02 12:18:10 -0600

    Adds index route and controller action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,10 @@
 class Api::V1::UsersController < ApplicationController
+
+  def index
+    users = User.all
+    render json: users.to_json
+  end
+
   def create
     user = User.new(
       first_name: params[:first_name], 
