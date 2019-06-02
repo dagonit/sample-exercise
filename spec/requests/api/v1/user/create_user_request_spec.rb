7751ea9b3a22f5c23c656c6ade532c2d20034f7c
abdullaqudrat <aqudrat630@gmail.com>
2019-06-02 11:30:14 -0600

    Adds user create route

--- spec/requests/api/v1/user/create_user_request_spec.rb
+++ spec/requests/api/v1/user/create_user_request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'User API' do
   it 'can create a user with required parameters' do
 
-    get '/api/v1/user?first_name=Abdulla&last_name=Qudrat&email=aqudrat638@gmail.com&social_security_number=555555555'
+    post '/api/v1/user?first_name=Abdulla&last_name=Qudrat&email=aqudrat638@gmail.com&social_security_number=555555555'
 
     response = JSON.parse(response.body, symbolize_names: true)
 
