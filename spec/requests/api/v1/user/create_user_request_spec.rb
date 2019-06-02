ce02c443a8800a9a17b2eafe3db9ca6a3de70244
abdullaqudrat <aqudrat630@gmail.com>
2019-06-02 13:52:43 -0600

    Adds ssn dash test in user create request spec

--- spec/requests/api/v1/user/create_user_request_spec.rb
+++ spec/requests/api/v1/user/create_user_request_spec.rb
@@ -30,4 +30,22 @@ describe 'User API' do
     expect(user_response).to have_key(:message)
     expect(user_response[:message]).to eq("Error")
   end
+  it 'can create a user with dashes in social security' do
+
+    post '/api/v1/users?first_name=Abdulla&last_name=Qudrat&email=aqudrat638@gmail.com&social_security_number=555-55-5555'
+    
+    user_response = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(response).to be_successful
+    expect(user_response).to be_a(Hash)
+    expect(user_response).to have_key(:id)
+    expect(user_response).to have_key(:first_name)
+    expect(user_response[:first_name]).to eq("Abdulla")
+    expect(user_response).to have_key(:last_name)
+    expect(user_response[:last_name]).to eq("Qudrat")
+    expect(user_response).to have_key(:email)
+    expect(user_response[:email]).to eq("aqudrat638@gmail.com")
+    expect(user_response).to have_key(:social_security_number)
+    expect(user_response[:social_security_number]).to eq("555555555")  
+  end
 end
