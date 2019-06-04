0ec64c8928621975b9cbfd8c04bb5f3fa1e711db
Anna Smolentzov <annasmolentzov@gmail.com>
2019-06-03 21:01:46 -0600

    Adds functionality to create action in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,13 @@ class UsersController < ApplicationController
   end
   
   def create
-    
+    user = User.create(user_params)
+    render json: user, status: :created
+  end
+  
+  private
+  
+  def user_params
+    params.permit(:first_name, :last_name, :email, :social_security_number)
   end
 end
