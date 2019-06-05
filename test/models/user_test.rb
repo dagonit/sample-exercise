560ce0c6c13ebc5b344ca61b87c261892b55e417
Anna Smolentzov <annasmolentzov@gmail.com>
2019-06-04 20:29:43 -0600

    Adds tests for SSN in either format

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,6 +7,14 @@ class UserTest < ActiveSupport::TestCase
     assert user.valid?
   end
   
+  test 'a user can be valid with SSN in either format' do
+    user = User.create(first_name: "First", last_name: "Last", email: "first@email.com", social_security_number: "111111111")
+    assert user.valid?
+    
+    user_with_hyphens = User.create(first_name: "First", last_name: "Last", email: "first@email.com", social_security_number: "111-11-1111")
+    assert user_with_hyphens.valid?
+  end
+  
   test 'a user cannot be created without all fields' do
     no_first_name = User.create(last_name: "Last", email: "first@email.com", social_security_number: "111111111")
     refute no_first_name.valid?
@@ -26,5 +34,8 @@ class UserTest < ActiveSupport::TestCase
   test 'a user cannot be created with an invalid SSN' do
     user = User.create(first_name: "First", last_name: "Last", email: "first@email.com", social_security_number: "111")
     refute user.valid?
+    
+    user_2 = User.create(first_name: "First", last_name: "Last", email: "first@email.com", social_security_number: "111-111111")
+    refute user_2.valid?
   end
 end
