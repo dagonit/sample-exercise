4254a5c61ff4c35d6ff356d9f6a74993a2cb372f
Anna Smolentzov <annasmolentzov@gmail.com>
2019-06-04 20:33:50 -0600

    Adds SSN format validation with or without hyphens

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
   validates_presence_of :first_name, :last_name, :email, :social_security_number
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
-  validates_format_of :social_security_number, with: /\d{9}/
+  validates_format_of :social_security_number, 
+                      with: ->(user) {
+                        if user.social_security_number && user.social_security_number.length == 9
+                          /\d{9}/
+                        else
+                          /\d{3}-\d{2}-\d{4}/
+                        end
+                        }
 end
