574ae2c6f43d848e936f59e0f4576759e675a0e7
Nico <nicovigil1@gmail.com>
2019-06-13 13:25:26 -0500

    adds test for user index action

--- spec/api/v1/requests/crud_users_spec.rb
+++ spec/api/v1/requests/crud_users_spec.rb
@@ -37,5 +37,28 @@ describe "User" do
       expect(response.body).to include(user.social_security_number)
     end
   end
+
+  context "Read" do
+    it 'can render a json of each user' do 
+      user1_credentials = {first_name: "Billy", 
+                          last_name: "Joel", 
+                          social_security_number: "123-45-6789",
+                          email: "email@gmail.com"}
+      user2_credentials = {first_name: "Elton", 
+                          last_name: "John", 
+                          social_security_number: "987654321",
+                          email: "tiny_danc3r@gmail.com"}
+
+      user1 = User.create(user1_credentials)
+      user2 = User.create(user2_credentials)
+
+      get api_v1_users_path
+
+      expect(response.body).to include(user1.email)
+      expect(response.body).to include(user1.social_security_number)
+      expect(response.body).to include(user2.email)
+      expect(response.body).to include(user2.social_security_number)
+    end
+  end
   
 end
