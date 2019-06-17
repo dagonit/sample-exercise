2616a3ebbd53ce7f13cf30ede245a8c291688533
April Dagonese <adagonese@gmail.com>
2019-06-17 13:27:37 -0600

    Adds test for valid user creation

---
+++ test/requests/api/v1/user_request_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe "Users API" do
+  it "returns a user upon valid user creation" do
+    post "/api/v1/users", params: {first_name: "John",
+                                   last_name: "Doe",
+                                   email: "jdoe@email.com",
+                                   social_security_number: "123456789"}
+    expect(response).to be_successful
+    expect(response.status).to eq(201)
+    body = JSON.parse(response.body, symbolize_names: true)
+    expect(body[:api_key]).to be_a(String)
+  end
+
+  it "returns an error code with invalid user creation" do
+    #user tries to register without a last name
+    post "/api/v1/users", params: {first_name: "John",
+                                   email: "jdoe@email.com",
+                                   social_security_number: "123456789"}
+    expect(response).to_not be_successful
+    body = JSON.parse(response.body, symbolize_names: true)
+    expect(body[:error]).to eq("User could not be saved with those credentials.")
+    expect(response.status).to eq(409)
+  end
+end
