eeaac08c59cc379efef838954ac729d8f1072b40
Teresa Knowles <teresa07moreno@gmail.com>
2019-07-12 13:25:26 -0600

    Add error handling

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,16 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    render json: User.create(user_params)
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: 201
+    else
+      render json: { errors: user.errors.full_messages }, status: 400
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :ssn)
+    params.require(:user).permit(:first_name, :last_name, :email, :ssn)
   end
 end
--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -1,15 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe "Users API", :type => :request do
-  it 'can create a new user' do
-    user_params = { first_name: "John", last_name: "Smith", email: "email@example.com", ssn: "000-00-0000"}
+  context 'with valid user information' do
+    it 'can create a new user' do
+      user_params = { first_name: "John", last_name: "Smith", email: "email@example.com", ssn: "000-00-0000"}
 
-    post "/api/v1/users", params: {user: user_params}
-    user = User.last
+      post "/api/v1/users", params: {user: user_params}
+      user = User.last
 
-    expect(response).to be_successful
-    expect(user.first_name).to eq(user_params[:first_name])
+      expect(response).to be_successful
+      expect(response.content_type).to eq("application/json")
+      expect(user.first_name).to eq(user_params[:first_name])
+    end
+
+    it 'returns the newly created user' do
+      user_params = { first_name: "John", last_name: "Smith", email: "email@example.com", ssn: "000-00-0000"}
+
+      post "/api/v1/users", params: {user: user_params}
+      expect(response).to have_http_status(201)
+    end
   end
+
+  context "with invalid user information" do
+    it 'cannot create a user' do
+      user_params = { first_name: "John", last_name: "Smith", email: "email@example.com"}
+      post "/api/v1/users", params: {user: user_params}
+
+      expect(response.content_type).to eq("application/json")
+      expect(response.status).to eq(400)
+      expect(JSON.parse(response.body)).to have_key("errors")
+
+    end
+  end
+
 
 
 
