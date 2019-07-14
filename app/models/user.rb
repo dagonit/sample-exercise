40f2ebf09113175552056d70bf2fed6372f7399a
github username <github email address>
2019-07-14 14:59:06 -0600

    added commit for social security number

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   # Regex to validate email is in format aaa@bbb.ccc
   validates_format_of :email, :with => /.+@.+\..+/i
+  # Regex to validate social security number is in format 0000000000 or 000-00-0000
   validates_format_of :social_security_number, :with => /\d{9}|\d{2}-\d{3}-\d{4}/
 end
