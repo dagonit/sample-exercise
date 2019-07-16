ebc5296b5469783bfa1971ce595666d27f755548
github username <jpsypek@gmail.com>
2019-07-16 14:17:35 -0600

    more detailed ss validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    render json: @users
+    render json: @users, status: 200
   end
 
   def create
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
       if @user.save
         render json: @user
       else
-        render :json => { :errors => @user.errors.full_messages }, :status => 400
+        render json: { :errors => @user.errors.full_messages }, status: 422
       end
   end
 
